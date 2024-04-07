2113f299e4bfd0800fa77cdf7c90c721453dd7ef
Ezeanaka Nathaniel Nwabueze <ezeanakanath@gmail.com>
2024-04-08 00:21:42 +0100

    Change status code for the deleteTask function

--- day_01/contollers/taskscontroller.js
+++ day_01/contollers/taskscontroller.js
@@ -55,9 +55,9 @@ const updateTask = async (req, res) => {
 const deleteTask = async (req, res) => {
 	try {
 		await Task.findByIdAndDelete(req.params.id);
-		res.status(200).json({ status: "success", data: null });
+		res.status(204).json({ status: "success", data: null });
 	} catch (err) {
-		res.status(400).json({
+		res.status(404).json({
 			status: "fail",
 			message: err.message,
 		});
