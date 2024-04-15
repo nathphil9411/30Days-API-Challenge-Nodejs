f3e7dc65a36acd371bbd364deb32c9a487a1de90
Ezeanaka Nathaniel Nwabueze <ezeanakanath@gmail.com>
2024-04-15 11:54:43 +0100

    Refactor api features
    create APIFeatures class for sorting, filtering and pagination
    update gitIgnore file

--- day_02/controller/todocontroller.js
+++ day_02/controller/todocontroller.js
@@ -1,4 +1,5 @@
 const Todo = require("./../models/todoModel");
+const APIFeatures = require("./../utils/apiFeatures");
 
 const createTodo = async (req, res) => {
 	try {
@@ -17,36 +18,14 @@ const createTodo = async (req, res) => {
 
 const getAllTodos = async (req, res) => {
 	try {
-		//filter according to categories
-		const queryObj = { ...req.query };
-		const excludedField = ["page", "sort", "limit", "fields"];
-		excludedField.forEach((el) => delete queryObj[el]);
-		let query = Todo.find(queryObj);
+		const features = new APIFeatures(Todo.find(), req.query)
+			.filter()
+			.sort()
+			.limit()
+			.paginate();
 
-		//Sort according to categories.
-		if (req.query.sort) {
-			const sortBy = req.query.sort.split(",").join(" ");
-			query = query.sort(sortBy);
-		} else {
-			query = query.sort("-createdAt");
-		}
-		//limiting the fields to be shown
-		if (req.query.fields) {
-			const fields = req.query.fields.split(",").join(" ");
-			query = query.select(fields);
-		} else {
-			query = query.select("-__v");
-		}
-		const page = req.query.page * 1 || 1;
-		const limit = req.query.limit * 1 || 10;
-		const skip = (page - 1) * limit;
-		query = query.skip(skip).limit(limit);
-		if (req.query.page) {
-			const allTodos = await Todo.countDocuments();
-			if (skip >= allTodos) throw new Error("this page does not exist");
-		}
-
-		const todos = await query;
+		const todos = await features.query;
+		console.log(todos.length);
 
 		res.status(200).json({ status: "success", data: { todos } });
 	} catch (err) {
